d80b0183897123067710af11ca23c74bd77da5c9
Akpolo Ogagaoghene Prince <ogazboizakpolo@gmail.com>
2026-03-31 17:08:46 +0100

    fix: rename conflicting migration to 1784000000014 and remove stray files

--- backend/src/app.with-swagger.ts
+++
@@ -1,8 +0,0 @@
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./config/swagger.esm.js";
-import app from "./app.js";
-
-// Mount Swagger docs route
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-export default app;
